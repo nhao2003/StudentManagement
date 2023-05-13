128f1fb5087c55387f1ce4ac3f9c6e424ba1ad59
Phan Hien <hienlephan2003@gmail.com>
2023-05-14 05:25:20 +0700

    Add School Year

---
+++ StudentManagement/Component/NewSchoolYear/UC/FirstUC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentManagement.Component.NewSchoolYear.UC
+{
+    /// <summary>
+    /// Interaction logic for FirstUC.xaml
+    /// </summary>
+    public partial class FirstUC : UserControl
+    {
+        public FirstUC()
+        {
+            InitializeComponent();
+        }
+    }
+}
