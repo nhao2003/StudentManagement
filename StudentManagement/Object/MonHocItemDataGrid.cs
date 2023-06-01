cad654a72f9c4406ec2dd19a9f98ae04dc9d2c76
nhao2003 <nhathaodev@gmail.com>
2023-06-01 13:41:05 +0700

    Fix checkbox in Datagridview

---
+++ StudentManagement/Object/MonHocItemDataGrid.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using StudentManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Object
+{
+    public partial class MonHocItemDataGrid : ObservableObject
+    {
+        [ObservableProperty]
+        private bool isCheckedMonHoc;
+        [ObservableProperty]
+        private string maMonHoc;
+        [ObservableProperty]
+        private string tenMonHoc;
+
+        private Monhoc monhoc;
+
+        [NotNull]
+        public Monhoc Monhoc
+        {
+            set { monhoc = value; }
+            get { return monhoc; }
+        }
+        public MonHocItemDataGrid(Monhoc monhoc)
+        {
+            isCheckedMonHoc = false;
+            maMonHoc = monhoc.Mamh;
+            tenMonHoc = monhoc.Tenmh;
+            Monhoc = monhoc;
+        }
+    }
+}
--- StudentManagement/ViewModel/RegulationViewModel.cs
+++ StudentManagement/ViewModel/RegulationViewModel.cs
@@ -3,7 +3,9 @@ using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using StudentManagement.Component.Regulation;
 using StudentManagement.Models;
+using StudentManagement.Object;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows;
@@ -22,27 +24,44 @@ namespace StudentManagement.ViewModel
         [ObservableProperty]
         private String tenMonHoc = "";
         [ObservableProperty]
-        private ObservableCollection<Monhoc> monhocs;
+        private ObservableCollection<MonHocItemDataGrid> monHocItems;
         [ObservableProperty]
         private Thamso[] thamsos = Array.Empty<Thamso>();
         [ObservableProperty]
         ContentControl contentControl = new();
 
         [ObservableProperty]
-        private Monhoc selectedMonHoc = new Monhoc();
+        private MonHocItemDataGrid? selectedMonHoc = null;
+        [ObservableProperty]
+        private bool isCheckAllItem = true;
 
         public RegulationViewModel()
         {
             contentControl.Content = new EmptyView();
             thamsos = DataProvider.ins.context.Thamsos.ToArray();
-            Monhocs = new ObservableCollection<Monhoc>(DataProvider.ins.context.Monhocs.Where(e => e.Isdeleted != null && !e.Isdeleted.Value));
+            MonHocItems = new ObservableCollection<MonHocItemDataGrid>();
+            foreach(Monhoc item in DataProvider.ins.context.Monhocs.Where(e => e.Isdeleted != null && !e.Isdeleted.Value))
+            {
+                MonHocItems.Add(new MonHocItemDataGrid(item));
+            }
         }
+
         [RelayCommand]
-        public void ShowDiaLog()
+        private void isCheckedAllChanged()
+        {
+            MessageBox.Show("Click");
+            foreach (MonHocItemDataGrid item in MonHocItems)
+            {
+                item.IsCheckedMonHoc = IsCheckAllItem;
+            }
+        }
+
+        [RelayCommand]
+        public void CapNhatThamSo()
         {
             try
             {
-                foreach (Thamso ts in thamsos)
+                foreach (Thamso ts in Thamsos)
                 {
                     if (!ValidDataType(ts.Giatri, ts.Typets))
                     {
@@ -65,16 +84,17 @@ namespace StudentManagement.ViewModel
         [RelayCommand]
         public void ThemMonHoc()
         {
-
+            MessageBox.Show(IsCheckAllItem.ToString());
+            return;
             try
             {
-
                 if (MaMonHoc.IsNullOrEmpty() || TenMonHoc.IsNullOrEmpty())
                 {
                     throw new Exception("Tên môn học hoặc mã môn học rỗng");
                 }
                 if (IsAddSubject)
                 {
+                    SelectedMonHoc = null;
                     Monhoc monhoc = new Monhoc
                     {
                         Mamh = MaMonHoc,
@@ -82,18 +102,20 @@ namespace StudentManagement.ViewModel
                     };
                     DataProvider.ins.context.Monhocs.Add(monhoc);
                     DataProvider.ins.context.SaveChanges();
+                    MonHocItems.Add(new MonHocItemDataGrid(monhoc));
                     MessageBox.Show("Đã thêm thành công");
                     MaMonHoc = "";
                     TenMonHoc = "";
                 }
                 else
                 {
-                    SelectedMonHoc.Tenmh = TenMonHoc;
-                    DataProvider.ins.context.Monhocs.Update(SelectedMonHoc);
+                    SelectedMonHoc.TenMonHoc = TenMonHoc;
+                    SelectedMonHoc.Monhoc.Tenmh = TenMonHoc;
+                    DataProvider.ins.context.Monhocs.Update(SelectedMonHoc.Monhoc);
                     DataProvider.ins.context.SaveChanges();
                     MessageBox.Show("Chỉnh sửa thành công");
                 }
-                Monhocs = new ObservableCollection<Monhoc>(DataProvider.ins.context.Monhocs.Where(e => e.Isdeleted != null && !e.Isdeleted.Value));
+                //TODO: Update gridview
             }
             catch (Exception ex)
             {
@@ -128,11 +150,14 @@ namespace StudentManagement.ViewModel
         [RelayCommand]
         private void showModifySubjectView()
         {
-            ContentControl.Content = new AddSubject();
-            IsAddSubject = false;
-            TitleAddSubject = "Chỉnh sửa môn học";
-            MaMonHoc = SelectedMonHoc.Mamh;
-            TenMonHoc = SelectedMonHoc.Tenmh;
+            if(SelectedMonHoc != null)
+            {
+                ContentControl.Content = new AddSubject();
+                IsAddSubject = false;
+                TitleAddSubject = "Chỉnh sửa môn học";
+                MaMonHoc = SelectedMonHoc.MaMonHoc;
+                TenMonHoc = SelectedMonHoc.TenMonHoc;
+            }
         }
         [RelayCommand]
         private void showAddSubjectView()
@@ -143,6 +168,31 @@ namespace StudentManagement.ViewModel
             MaMonHoc = "";
             TenMonHoc = "";
         }
+        [RelayCommand]
+        private void XoaMonHoc()
+        {
+            MessageBoxResult result = MessageBox.Show("Bạn có chắc chắn muốn xóa những môn học này?", "Xác nhận xóa", MessageBoxButton.YesNo, MessageBoxImage.Question);
+            if (result == MessageBoxResult.Yes)
+            {
+                List<MonHocItemDataGrid> monHocToRemove = new List<MonHocItemDataGrid>();
 
+                foreach (MonHocItemDataGrid monHoc in MonHocItems)
+                {
+                    if (monHoc.IsCheckedMonHoc)
+                    {
+                        monHocToRemove.Add(monHoc);
+                        monHoc.Monhoc.Isdeleted = true;
+                        DataProvider.ins.context.Update(monHoc.Monhoc);
+                    }
+                }
+                DataProvider.ins.context.SaveChanges();
+                foreach (MonHocItemDataGrid monHoc in monHocToRemove)
+                {
+                    MonHocItems.Remove(monHoc);
+                }
+
+                MessageBox.Show("Xóa thành công!", "Thành công", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+        }
     }
 }
