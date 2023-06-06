ab7c8f6f42a26a025289ca516a54a23d6208c0e9
4nh3k <21520579@gm.uit.edu.vn>
2023-06-06 13:08:19 +0700

    regen models

--- StudentManagement/Models/Diemmonhoc.cs
+++ StudentManagement/Models/Diemmonhoc.cs
@@ -1,11 +1,14 @@
-﻿namespace StudentManagement.Models
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentManagement.Models
 {
     public partial class Diemmonhoc
     {
         public string Mahs { get; set; } = null!;
         public string Mahk { get; set; } = null!;
         public string Mamh { get; set; } = null!;
-        public string Manh { get; set; }
+        public string Manh { get; set; } = null!;
         public string Malkt { get; set; } = null!;
         public double Diem { get; set; }
 
@@ -13,6 +16,6 @@
         public virtual Hocsinh MahsNavigation { get; set; } = null!;
         public virtual Loaikiemtra MalktNavigation { get; set; } = null!;
         public virtual Monhoc MamhNavigation { get; set; } = null!;
-        public virtual Namhoc ManhNavigation { get; set; }
+        public virtual Namhoc ManhNavigation { get; set; } = null!;
     }
 }
